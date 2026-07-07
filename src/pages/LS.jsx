d0107270ba7f4d00ebce5e98a635b9f0a074f3fc
Vadim <vadim@MacBook-Pro-Vadim.local>
2026-07-07 19:47:37 +0300

    Добавлен параметр embed для Streamlit

--- src/pages/LS.jsx
+++ src/pages/LS.jsx
@@ -4,7 +4,7 @@ const LS = () => {
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       <iframe
-        src="https://zhd-app-ltqtqqjz4zgnvd3wbqgj48.streamlit.app/"
+        src="https://zhd-app-ltqtqqjz4zgnvd3wbqgj48.streamlit.app/?embed=true"
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="Журнал ШЧ"
         allow="fullscreen"
