2b021ea82f9ab09b53dea82d061d4a07c080211f
Vadim <vadim@MacBook-Pro-Vadim.local>
2026-07-07 22:01:52 +0300

    Смена ссылки на Render

--- src/pages/LS.jsx
+++ src/pages/LS.jsx
@@ -4,7 +4,7 @@ const LS = () => {
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       <iframe
-        src="https://zhd-app-ltqtqqjz4zgnvd3wbqgj48.streamlit.app/?embed=true"
+        src="https://zhd-streamlit.onrender.com"
         style={{ width: '100%', height: '100%', border: 'none' }}
         title="Журнал ШЧ"
         allow="fullscreen"
